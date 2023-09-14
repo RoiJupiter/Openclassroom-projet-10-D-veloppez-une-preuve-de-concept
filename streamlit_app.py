4dbd99fb12a592498a303afcad94a58ca37bcdaa
Jupiter <varrat.asanka@gmail.com>
2023-09-14 11:29:43 +0200

    mise a jour du data vizualisation

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,6 @@ import pandas as pd
 
 from function_predict import predict
 
-from keras_preprocessing.sequence import pad_sequences
-from tensorflow import keras
-
-import pickle
-from keras import backend as K
-
 # Titre de l'application
 st.title('Analyse de Sentiment des Tweets')
 
@@ -26,24 +20,31 @@ if st.button('Analyser le Sentiment'):
     else:
         st.warning('Veuillez saisir un tweet pour l\'analyse.')
 
-
 # Titre de l'application
 st.title("Data Analyse")
 
-# Chargez et affichez l'image PNG
-image = "class_distribution.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
-st.image(image, caption="Distribution de la classe target", use_column_width=True)
 
 # Titre de l'application
-st.title("Affichage de l'ancien Wordcloud")
+st.title("Affichage du Wordcloud du modèle Keras")
 
 # Chargez et affichez l'image PNG
-wordwloud = "wordcloud.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
+wordwloud = "wordcloud1.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
 st.image(wordwloud, caption="Wordcloud du dataset", use_column_width=True)
 
 # Titre de l'application
-st.title("Affichage du nouveau Wordcloud")
+st.title("Affichage des Wordclouds du modèles ROBERTa enfonction de la classe")
 
-# Chargez et affichez l'image PNG
-wordcloud2 = "wordcloud2.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
-st.image(wordcloud2, caption="Wordcloud du dataset", use_column_width=True)
+# Créer deux colonnes
+col1, col2, col3 = st.columns(3)
+
+# Charger et afficher la première image PNG (Wordcloud Neutre)
+wordcloud_neutre = "wordcloud2.png"  # Remplacez par le chemin vers votre image PNG Neutre
+col1.image(wordcloud_neutre, caption="Wordcloud des mots Positif", use_column_width=True)
+
+# Charger et afficher la première image PNG (Wordcloud Neutre)
+wordcloud_neutre = "wordcloud3.png"  # Remplacez par le chemin vers votre image PNG Neutre
+col2.image(wordcloud_neutre, caption="Wordcloud des mots Neutre", use_column_width=True)
+
+# Charger et afficher la deuxième image PNG (Wordcloud Négatif)
+wordcloud_negatif = "wordcloud4.png"  # Remplacez par le chemin vers votre image PNG Négatif
+col3.image(wordcloud_negatif, caption="Wordcloud des mots Négatif", use_column_width=True)
