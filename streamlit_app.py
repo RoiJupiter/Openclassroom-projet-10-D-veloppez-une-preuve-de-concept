7df2478af76020c5f618a1514ca09aedfbcd6e8f
Jupiter <varrat.asanka@gmail.com>
2023-09-14 17:44:04 +0200

    push master

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,20 +84,28 @@ if st.button('Analyser le Sentiment'):
 # Titre de l'application
 st.title("Data Analyse")
 
-# Chargez et affichez l'image PNG
-image = "class_distribution.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
-st.image(image, caption="Distribution de la classe target", use_column_width=True)
 
 # Titre de l'application
-st.title("Affichage de l'ancien Wordcloud")
+st.title("Affichage du Wordcloud du modèle Keras")
 
 # Chargez et affichez l'image PNG
-wordwloud = "wordcloud.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
+wordwloud = "wordcloud1.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
 st.image(wordwloud, caption="Wordcloud du dataset", use_column_width=True)
 
 # Titre de l'application
-st.title("Affichage du nouveau Wordcloud")
+st.title("Affichage des Wordclouds du modèles ROBERTa enfonction de la classe")
 
-# Chargez et affichez l'image PNG
-wordcloud2 = "wordcloud2.png"  # Remplacez "mon_graphique.png" par le chemin vers votre image PNG
-st.image(wordcloud2, caption="Wordcloud du dataset", use_column_width=True)
+# Créer deux colonnes
+col1, col3 = st.columns(2)
+
+# Charger et afficher la première image PNG (Wordcloud Neutre)
+wordcloud_neutre = "wordcloud2.png"  # Remplacez par le chemin vers votre image PNG Neutre
+col1.image(wordcloud_neutre, caption="Wordcloud des mots Positif", use_column_width=True)
+
+var = '''# Charger et afficher la première image PNG (Wordcloud Neutre)
+wordcloud_neutre = "wordcloud3.png"  # Remplacez par le chemin vers votre image PNG Neutre
+col2.image(wordcloud_neutre, caption="Wordcloud des mots Neutre", use_column_width=True)'''
+
+# Charger et afficher la deuxième image PNG (Wordcloud Négatif)
+wordcloud_negatif = "wordcloud4.png"  # Remplacez par le chemin vers votre image PNG Négatif
+col3.image(wordcloud_negatif, caption="Wordcloud des mots Négatif", use_column_width=True)
